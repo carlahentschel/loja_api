43650db3aa7a01aa90b96677bf9944eeddee2664
Bowie <carlarhentschel@gmail.com>
2024-03-12 15:05:31 -0300

    refatora controller de produto

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProdutoRepository } from './produto.repository';
+import { CriaProdutoDTO } from './dto/CriaProduto.dto';
 
 @Controller('/produtos')
 export class ProdutoController {
   constructor(private produtoRepository: ProdutoRepository) {}
 
   @Post()
-  async criaProduto(@Body() dadosProduto) {
+  async criaProduto(@Body() dadosProduto: CriaProdutoDTO) {
     this.produtoRepository.salvar(dadosProduto);
     return dadosProduto;
   }
