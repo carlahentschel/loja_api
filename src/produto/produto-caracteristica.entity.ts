06932025996bac6e1add10c15e29e3e9f1605659
Bowie <carlarhentschel@gmail.com>
2024-03-14 10:06:52 -0300

    adiciona campo id

--- src/produto/produto-caracteristica.entity.ts
+++ src/produto/produto-caracteristica.entity.ts
@@ -1,7 +1,9 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'produto_caracteristicas' })
 export class ProdutoCaracteristica {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
   @Column({ name: 'nome', length: 100, nullable: false })
   nome: string;
   @Column({ name: 'descricao', length: 100, nullable: false })
--- src/produto/produto-imagem.entity.ts
+++ src/produto/produto-imagem.entity.ts
@@ -1,7 +1,9 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'produto_imagens' })
 export class ImagemProduto {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
   @Column({ name: 'url', length: 100, nullable: false })
   url: string;
   @Column({ name: 'descricao', length: 100, nullable: false })
